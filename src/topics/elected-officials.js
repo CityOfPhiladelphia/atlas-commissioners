9ca05d453bc4d6a1305093a490985706c550291e
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-03-20 14:10:13 -0400

    makes elected officials term lengths different

--- src/topics/elected-officials.js
+++ src/topics/elected-officials.js
@@ -5,7 +5,7 @@ const titleCase = transforms.titleCase.transform;
 const nth = transforms.nth.transform;
 let $t;
 
-function formatMember(person, districtLabel) {
+function formatMember(person, termLength, districtLabel) {
   
   const website = '<a href="https://' + person.website + '" target="_blank">' + person.first_name +" " + person.last_name + "</a>";
   
@@ -36,7 +36,7 @@ function formatMember(person, districtLabel) {
   const phone2 = phone(person.main_contact_phone_2) || '';
   const fax = 'F: '+ phone(person.main_contact_fax) || '';
   const email = '<b><a href=mailto:"' + person.email + '">' + person.email + '</a></b>';
-  const term = 'Current Term: ' + (person.next_election-4) + ' - ' + person.next_election;
+  const term = 'Current Term: ' + (person.next_election-termLength) + ' - ' + person.next_election;
 
   let returnString = website;
   if (district) {
@@ -103,7 +103,7 @@ export default {
             value: function(state) {
               const councilMember = state.sources.electedOfficials.data.rows.filter(person => person.office_label == "City Council");
               const districtLabel = 'Council District';
-              return formatMember(councilMember[0], districtLabel);
+              return formatMember(councilMember[0], 4, districtLabel);
             },
           },
           {
@@ -112,7 +112,7 @@ export default {
               const councilAtLarge = state.sources.electedOfficials.data.rows.filter(person => person.office == "city_council_at_large");
               let theString = '';
               for (const [ index, councilMember ] of councilAtLarge.entries()) {
-                theString += formatMember(councilMember);
+                theString += formatMember(councilMember, 4);
                 index < councilAtLarge.length - 1 ? theString += '<br><br>' : theString += '';
               }
               return theString;
@@ -129,14 +129,14 @@ export default {
             label: 'electedOfficials.topic.verticalTable1.districtAttorney',
             value: function(state) {
               const districtAttorney = state.sources.electedOfficials.data.rows.filter(person => person.office == "district_attorney");
-              return formatMember(districtAttorney[0]);
+              return formatMember(districtAttorney[0], 4);
             },
           },
           {
             label: 'electedOfficials.topic.verticalTable1.controller',
             value: function(state) {
               const controller = state.sources.electedOfficials.data.rows.filter(person => person.office == "city_controller");
-              return formatMember(controller[0]);
+              return formatMember(controller[0], 4);
             },
           },
           {
@@ -146,7 +146,7 @@ export default {
               let theString = '';
               for (const [ index, commissioner ] of commissioners.entries()) {
                 if (commissioner.last_name != null) {
-                  theString += formatMember(commissioner);
+                  theString += formatMember(commissioner, 4);
                   index < commissioners.length - 1 ? theString += '<br><br>' : theString += '';
                 }
               }
@@ -157,14 +157,14 @@ export default {
             label: 'electedOfficials.topic.verticalTable1.sheriff',
             value: function(state) {
               const sheriff = state.sources.electedOfficials.data.rows.filter(person => person.office == "sheriff");
-              return formatMember(sheriff[0]);
+              return formatMember(sheriff[0], 4);
             },
           },
           {
             label: 'electedOfficials.topic.verticalTable1.registerOfWills',
             value: function(state) {
               const registerOfWills = state.sources.electedOfficials.data.rows.filter(person => person.office == "register_of_wills");
-              return formatMember(registerOfWills[0]);
+              return formatMember(registerOfWills[0], 4);
             },
           },
         ],
@@ -183,7 +183,7 @@ export default {
             value: function(state) {
               const stateHouse = state.sources.electedOfficials.data.rows.filter(person => person.office == "state_house");
               const districtLabel = 'District';
-              return formatMember(stateHouse[0], districtLabel);
+              return formatMember(stateHouse[0], 4, districtLabel);
             },
           },
           {
@@ -191,42 +191,42 @@ export default {
             value: function(state) {
               const stateSenate = state.sources.electedOfficials.data.rows.filter(person => person.office == "state_senate");
               const districtLabel = 'District';
-              return formatMember(stateSenate[0], districtLabel);
+              return formatMember(stateSenate[0], 4, districtLabel);
             },
           },
           {
             label: 'electedOfficials.topic.verticalTable2.governor',
             value: function(state) {
               const governor = state.sources.electedOfficials.data.rows.filter(person => person.office == "governor");
-              return formatMember(governor[0]);
+              return formatMember(governor[0], 4);
             },
           },
           {
             label: 'electedOfficials.topic.verticalTable2.lieutenantGovernor',
             value: function(state) {
               const ltGovernor = state.sources.electedOfficials.data.rows.filter(person => person.office == "lt_governor");
-              return formatMember(ltGovernor[0]);
+              return formatMember(ltGovernor[0], 4);
             },
           },
           {
             label: 'electedOfficials.topic.verticalTable2.attorneyGeneral',
             value: function(state) {
               const attorneyGeneral = state.sources.electedOfficials.data.rows.filter(person => person.office == "attorney_general");
-              return formatMember(attorneyGeneral[0]);
+              return formatMember(attorneyGeneral[0], 4);
             },
           },
           {
             label: 'electedOfficials.topic.verticalTable2.stateTreasurer',
             value: function(state) {
               const stateTreasurer = state.sources.electedOfficials.data.rows.filter(person => person.office == "state_treasurer");
-              return formatMember(stateTreasurer[0]);
+              return formatMember(stateTreasurer[0], 4);
             },
           },
           {
             label: 'electedOfficials.topic.verticalTable2.auditorGeneral',
             value: function(state) {
               const auditorGeneral = state.sources.electedOfficials.data.rows.filter(person => person.office == "auditor_general");
-              return formatMember(auditorGeneral[0]);
+              return formatMember(auditorGeneral[0], 4);
             },
           },
         ],
@@ -244,7 +244,7 @@ export default {
             label: 'electedOfficials.topic.verticalTable3.congressionalRepresentative',
             value: function(state) {
               const usHouse = state.sources.electedOfficials.data.rows.filter(person => person.office == "us_house");
-              return formatMember(usHouse[0]);
+              return formatMember(usHouse[0], 2);
             },
           },
           {
@@ -255,7 +255,7 @@ export default {
               // for (let senator of senators) {
               for (const [ index, senator ] of senators.entries()) {
                 if (senator.last_name != null) {
-                  theString += formatMember(senator);
+                  theString += formatMember(senator, 6);
                   index < senators.length - 1 ? theString += '<br><br>' : theString += '';
                 }
               }
